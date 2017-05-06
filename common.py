2ed15450e2b34a10f02ccf3fa07f5661a14d205b
jpceia <joao.p.ceia@gmail.com>
2017-05-06 23:54:42 +0100

    changes in common.py file

--- common.py
+++ common.py
@@ -32,37 +32,40 @@ class CacheWrap:
 
 def call_func(f):
     @wraps(f)
-    def wrapper(obj, params=None):
+    def wrapper(obj, params=None, **kwargs):
         if params is None:
             params = []
-        return f(obj, params)
+        return f(obj, params, **kwargs)
     return wrapper
 
 
 def vector_func(g):
     @wraps(g)
-    def wrapper(obj, params=None, i=None, diag=False):
+    def wrapper(obj, params=None, i=None,
+                diag=False, **kwargs):
         if params is None:
             params = []
         if i is not None:
-            return g(obj, params, i, diag=diag)
+            return g(obj, params, i, diag=diag, **kwargs)
         else:
-            return map(lambda k: g(obj, params, k, diag=diag),
+            return map(lambda k: g(obj, params, k, diag=diag, **kwargs),
                        range(len(params)))
     return wrapper
 
 
 def matrix_func(h):
     @wraps(h)
-    def wrapper(obj, params=None, i=None, j=None, diag_i=False, diag_j=False):
+    def wrapper(obj, params=None, i=None, j=None,
+                diag_i=False, diag_j=False, **kwargs):
         if params is None:
             params = []
         if i is not None and j is not None:
-            return h(obj, params, i, j, diag_i=diag_i, diag_j=diag_j)
+            return h(obj, params, i, j, diag_i=diag_i, diag_j=diag_j, **kwargs)
         else:
             return map(lambda k:
                        map(lambda l:
-                           h(obj, params, k, l, diag_i=diag_i, diag_j=diag_j),
+                           h(obj, params, k, l,
+                             diag_i=diag_i, diag_j=diag_j, **kwargs),
                            range(k + 1)), range(len(params)))
     return wrapper
 
@@ -78,30 +81,31 @@ def vector_sum(vector, params, param_feat, i):
 def matrix_sum(matrix, params, param_feat, i, j):
     param_feat_i = param_feat.get(i, [])
     param_feat_j = param_feat.get(j, [])
-    pdim_i = params[i].ndim
-    pdim_j = params[j].ndim
-    for k in range(pdim_i):
-        matrix = matrix.swapaxes(
-            k + pdim_j,
-            param_feat_i[k] + pdim_i + pdim_j)
-    for k in range(pdim_j):
+    p_i = params[i].ndim
+    p_j = params[j].ndim
+    for k in range(p_i):
+        matrix = matrix.swapaxes(p_j + k, p_i + p_j + param_feat_i[k])
+    for k in range(p_j):
         f = param_feat_j[k]
         if f in param_feat_i:
             idx = np.zeros(matrix.ndim, dtype=np.bool)
             idx[param_feat_j.index(f)] = True
-            idx[pdim_j + k] = True
+            idx[p_j + k] = True
             idx = map(lambda b: slice(None) if b else None, idx)
-            matrix = matrix * np.eye(matrix.shape[pdim_j + k])[idx]
+            matrix = matrix * np.eye(matrix.shape[p_j + k])[idx]
         else:
-            matrix = matrix.swapaxes(k, f + pdim_i + pdim_j)
-    return matrix.sum(tuple(np.arange(pdim_i + pdim_j, matrix.ndim)))
+            matrix = matrix.swapaxes(k, p_i + p_j + f)
+    return matrix.sum(tuple(np.arange(p_i + p_j, matrix.ndim)))
 
 
-def transpose(result, params, i, j):
-    slc = [slice(None)] * params[i].ndim
-    slc += [None] * params[j].ndim
-    slc += [Ellipsis]
-    return result[i][slc]
+def transpose(vector, params, i, j):
+    if params[i].ndim > vector[i].ndim:
+        return np.zeros(())
+    else:
+        slc = [slice(None)] * params[i].ndim
+        slc += [None] * params[j].ndim
+        slc += [Ellipsis]
+        return vector[i][slc]
 
 
 class IndexMap(list):
