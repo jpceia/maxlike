7918a4bc0971b0ab420e663d6aac9152c5857a19
jpceia <joao.p.ceia@gmail.com>
2018-04-27 23:54:47 +0100

    added test_finite and edited arguments of Sum.add method

--- tests/tests.py
+++ tests/tests.py
@@ -4,17 +4,18 @@ import numpy as np
 # import sys
 # sys.path.insert(0, "..")
 import maxlike
-from maxlike.func import Encode, Vector, Linear, Quadratic
+from maxlike.func import Encode, Vector, Linear, Quadratic, Compose, Exp, Poisson
 
 
 class Test(unittest.TestCase):
+
     def test_poisson(self):
         mle = maxlike.Poisson()
         mle.model = maxlike.func.Sum(3)
-        mle.model.add(0, 0, Encode())
-        mle.model.add(1, 1, -Encode())
+        mle.model.add(Encode(), 0, 0)
+        mle.model.add(-Encode(), 1, 1)
+        mle.model.add(Vector(np.arange(2) - .5), 2, 2)
         mle.add_constraint([0, 1], Linear([1, 1]))
-        mle.model.add(2, 2, Vector(np.arange(2) - .5))
         g = pd.read_csv("test_data1.csv", index_col=[0, 1, 2])['g']
         prepared_data, _ = maxlike.utils.prepare_series(
             g, {'N': np.size, 'X': np.sum})
@@ -36,6 +37,7 @@ class Test(unittest.TestCase):
         self.assertAlmostEqual(h.data, 0.3496149212379256, delta=tol)
         self.assertAlmostEqual(s_h, 0.0804417430337, delta=tol)
         df = pd.read_csv("test_results1.csv")
+        print(a)
         self.assertTrue(np.allclose(a, df['a'].values, atol=tol))
         self.assertTrue(np.allclose(b, df['b'].values, atol=tol))
         self.assertTrue(np.allclose(s_a, df['s_a'].values, atol=tol))
@@ -44,8 +46,8 @@ class Test(unittest.TestCase):
     def test_poisson_reg(self):
         mle = maxlike.Poisson()
         mle.model = maxlike.func.Sum(2)
-        mle.model.add(0, 0, Encode())
-        mle.model.add(1, 1, -Encode())
+        mle.model.add(Encode(), 0, 0)
+        mle.model.add(-Encode(), 1, 1)
         mle.add_constraint([0, 1], Linear([1, 1]))
         mle.add_regularization([0, 1], Quadratic(0, 1))
         g = pd.read_csv("test_data1.csv", index_col=[0, 1])['g']
@@ -69,8 +71,8 @@ class Test(unittest.TestCase):
     def test_logistic(self):
         mle = maxlike.Logistic()
         mle.model = maxlike.func.Sum(2)   # Exp ?
-        mle.model.add(0, 0, Encode())
-        mle.model.add(1, 1, -Encode())
+        mle.model.add(Encode(), 0, 0)
+        mle.model.add(-Encode(), 1, 1)
         mle.add_constraint([0, 1], Linear([1, 1]))
         g = pd.read_csv("test_data1.csv", index_col=[0, 1])['g'] > 1
         prepared_data, _ = maxlike.utils.prepare_series(
@@ -93,8 +95,8 @@ class Test(unittest.TestCase):
     def test_negative_binomial(self):
         mle = maxlike.NegativeBinomial()
         mle.model = maxlike.func.Sum(2)
-        mle.model.add(0, 0, Encode())
-        mle.model.add(1, 1, -Encode())
+        mle.model.add(Encode(), 0, 0)
+        mle.model.add(-Encode(), 1, 1)
         mle.add_constraint([0, 1], Linear([1, 1]))
         g = pd.read_csv("test_data1.csv", index_col=[0, 1])['g']
         prepared_data, _ = maxlike.utils.prepare_series(
@@ -109,6 +111,32 @@ class Test(unittest.TestCase):
         a, b = mle.params_
         s_a, s_b = mle.std_error()
         df = pd.read_csv("test_negative_binomial.csv")
+        self.assertTrue(np.allclose(a, df['a'].values, atol=tol))
+        self.assertTrue(np.allclose(b, df['b'].values, atol=tol))
+        self.assertTrue(np.allclose(s_a, df['s_a'].values, atol=tol))
+        self.assertTrue(np.allclose(s_b, df['s_b'].values, atol=tol))
+
+    def test_finite(self):
+        L = 10
+        mle = maxlike.Finite()
+        foo = maxlike.func.Sum(2)
+        foo.add(Encode(), 0, 0)
+        foo.add(-Encode(), 1, 1)
+        mle.model = Compose(Poisson(L), Compose(Exp(), foo))
+        mle.add_constraint([0, 1], Linear([1, 1]))
+        g = pd.read_csv("test_data1.csv", index_col=[0, 1])['g']
+        log_mean = np.log(g.mean()) / 2
+        a = g.groupby(level='t1').mean().map(np.log) - log_mean
+        b = log_mean - g.groupby(level='t2').mean()
+        mle.add_param(a)
+        mle.add_param(b)
+        tol = 1e-8
+        prepared_data, _ = maxlike.utils.prepare_series(
+            maxlike.utils.df_count(g, L).stack(), {'N': np.sum})
+        mle.fit(tol=tol, **prepared_data)
+        a, b = mle.params_
+        s_a, s_b = mle.std_error()
+        df = pd.read_csv("test_finite.csv")
         self.assertTrue(np.allclose(a, df['a'].values, atol=tol))
         self.assertTrue(np.allclose(b, df['b'].values, atol=tol))
         self.assertTrue(np.allclose(s_a, df['s_a'].values, atol=tol))
