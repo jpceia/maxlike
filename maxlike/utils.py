acf8170948bd126ef152f789d53b45232b70c072
joao ceia <joao.p.ceia@gmail.com>
2018-05-30 23:23:53 +0100

    added add_axis option to prepare_series function

--- maxlike/utils.py
+++ maxlike/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-def prepare_series(observations, transformations=None):
+def prepare_series(observations, transformations=None, add_axis=None):
     """
     Extracts the relevant parameters from a Series of observations to feed the
     maxlike object.
@@ -15,6 +15,9 @@ def prepare_series(observations, transformations=None):
     transformations : dict
         (named) list of transformations to apply to observations labels,
         grouped by index
+    add_axis: dict, list
+        Different axis to use. Dictionary if the observations have multiIndex,
+        list if vanilla index
 
     Returns
     -------
@@ -28,8 +31,11 @@ def prepare_series(observations, transformations=None):
         transformations = {"N": np.size}
 
     if isinstance(observations.index, pd.MultiIndex):
-        axis = tuple((level.sort_values()
-                      for level in observations.index.levels))
+        axis = [level.sort_values() for level in observations.index.levels]
+        if add_axis is not None:
+            for k, level in enumerate(observations.index.levels):
+                if level.name in add_axis:
+                    axis[k] = sorted(set(axis[k]).union(add_axis[level.name]))
         shape = tuple((len(a) for a in axis))
         df = observations.groupby(observations.index).\
             agg(transformations.values()).\
@@ -37,9 +43,11 @@ def prepare_series(observations, transformations=None):
                             for name, transf in transformations.items()}).\
             reindex(pd.MultiIndex.from_product(axis)).fillna(0)
     else:
-        axis = observations.index.sort_values()
-        shape = (axis.size)
-        df = observations.groupby(axis).agg(transformations.values()).\
+        axis = sorted(observations.index.unique())    
+        if add_axis is not None:
+            axis = sorted(set(axis).union(add_axis))
+        shape = (len(axis))
+        df = observations.groupby(observations.index).agg(transformations.values()).\
             rename(columns={transf.__name__: name
                             for name, transf in transformations.items()}).\
             reindex(axis).fillna(0)
@@ -52,9 +60,8 @@ def prepare_dataframe(df, weight_col, result_col, transformations,
     axis = [level.sort_values() for level in df.index.levels]
     if add_axis is not None:
         for k, level in enumerate(df.index.levels):
-            name = level.name
-            if name in add_axis:
-                axis[k] = sorted(set(axis[k]).union(add_axis[name]))
+            if level.name in add_axis:
+                axis[k] = sorted(set(axis[k]).union(add_axis[level.name]))
     shape = tuple((len(a) for a in axis))
     new_index = pd.MultiIndex.from_product(axis)
     w = df[weight_col].to_frame('N').groupby(df.index).sum().\
