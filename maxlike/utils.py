f375219800ab8e3a8b8c90cf7362a7a835386f7f
Joao CEIA <joao.p.ceia@gmail.com>
2018-08-28 17:31:59 +0100

    added num_hess in utils.py, removed set_params option

--- maxlike/utils.py
+++ maxlike/utils.py
@@ -89,17 +89,43 @@ def df_count(series, size, name=None):
         columns=pd.Index(np.arange(size), name=name))
 
 
-def num_grad(mle, params, e=1e-6, set_params=False):
-    if set_params:
-        mle.reset_param()
-        for p in params:
-            mle.add_param(p, False)
-    flat_params = np.concatenate([p[~p_.mask] for p, p_ in zip(params, mle.params)])
+def num_grad(mle, params, e=1e-6):
+
+    flat_params = np.concatenate([
+        np.asarray(p)[~p_.mask] for p, p_ in zip(params, mle.params)])
     like_0 = float(mle.like(params).values)
     num_grad = np.zeros_like(flat_params)
+
     for i in range(len(flat_params)):
         bump = np.zeros_like(flat_params)
         bump[i] = e
         bumped_params = mle._reshape_params(flat_params + bump)
         num_grad[i] = (float(mle.like(bumped_params).values) - like_0) / e
+
     return mle._reshape_array(num_grad)
+
+
+def num_hess(mle, params, e=1e-6):
+
+    flat_params = np.concatenate([
+        np.asarray(p)[~p_.mask] for p, p_ in zip(params, mle.params)])
+    like_0 = mle.like(params).values
+    like_i = np.zeros_like(flat_params)
+    n = len(flat_params)
+
+    for i in range(n):
+        bump = np.zeros_like(flat_params)
+        bump[i] = e
+        bumped_params = mle._reshape_params(flat_params + bump)
+        like_i[i] = mle.like(bumped_params).values
+    num_hess = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            bump = np.zeros_like(flat_params)
+            bump[i] = e
+            bump[j] = e
+            bumped_params = mle._reshape_params(flat_params + bump)
+            num_hess[i][j] = (mle.like(bumped_params).values
+                              + like_0 - like_i[i] - like_i[j]) / e / e
+    return mle._reshape_matrix(np.array(num_hess))
