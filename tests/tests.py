d0794e18200a44b2bc0249feba63f852cfedaccb
Joao CEIA <joao.p.ceia@gmail.com>
2018-08-29 15:43:09 +0100

    changed test_logistic to use new static files

--- tests/tests.py
+++ tests/tests.py
@@ -193,23 +193,30 @@ class Test(unittest.TestCase):
 
     def test_logistic(self):
         mle = maxlike.Logistic()
-        mle.model = Sum(2)
+        mle.model = Sum(3)
         mle.model.add(X(), 0, 0)
         mle.model.add(-X(), 1, 1)
+        mle.model.add(Vector(np.arange(2) - .5), 2, 2)
         mle.add_constraint([0, 1], Linear([1, 1]))
-        g = pd.read_csv(r"data\test_data1.csv", index_col=[0, 1])['g'] > 1
+        g = pd.read_csv(r"data\data1.csv", index_col=[0, 1, 2])['g'] > 0
         prepared_data, _ = maxlike.utils.prepare_series(
             g, {'N': np.size, 'X': np.sum})
-        m = np.log(g.mean())
-        a = np.log(g.groupby(level='t1').mean()) - m
-        b = m - np.log(g.groupby(level='t2').mean())
+        h = g.groupby(level='h').mean().map(np.log).reset_index().prod(1).sum()
+        log_mean = np.log(g.mean())
+        a = np.log(g.groupby(level='t1').mean()) - log_mean
+        b = log_mean - np.log(g.groupby(level='t2').mean())
         mle.add_param(a)
         mle.add_param(b)
+        mle.add_param(h)
         tol = 1e-8
         mle.fit(tol=tol, **prepared_data)
-        a, b = mle.params
-        s_a, s_b = mle.std_error()
-        df = pd.read_csv(r"data\test_results_logistic.csv")
+        a, b, h = mle.params
+        s_a, s_b, s_h = mle.std_error()
+
+        self.assertAlmostEqual(h,   0.4060378612453205, delta=tol)
+        self.assertAlmostEqual(s_h, 0.13212794903767752, delta=tol)
+
+        df = pd.read_csv(r"data\test_logistic.csv")
         self.assertTrue(np.allclose(a, df['a'].values, atol=tol))
         self.assertTrue(np.allclose(b, df['b'].values, atol=tol))
         self.assertTrue(np.allclose(s_a, df['s_a'].values, atol=tol))
