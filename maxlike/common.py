021fa98eb40ef1d91315eab83b1da80575629730
Joao CEIA <joao.p.ceia@gmail.com>
2018-09-10 10:47:05 +0100

    made Param hashable

--- maxlike/common.py
+++ maxlike/common.py
@@ -2,16 +2,22 @@ from __future__ import print_function
 import numpy as np
 from hashlib import sha1
 from scipy.special import factorial
+from random import getrandbits
 
 
 class Param(np.ma.MaskedArray):
+    def __new__(cls, data, *args, **kwargs):
+        obj = super(Param, cls).__new__(cls, data, *args, **kwargs)
+        obj.hash = getrandbits(128)
+        return obj
+
     def reset(self, data, mask=None):
         self.data[:] = data
         if mask is not None:
             self.mask = mask
 
     def __hash__(self):
-        return int(sha1(self.data).hexdigest(), 16)
+        return self.hash
 
 
 class IndexMap(list):
