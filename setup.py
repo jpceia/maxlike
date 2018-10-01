5fc35f5c9dfa02aa98568351b5b02d7a32f32379
Joao CEIA <joao.p.ceia@gmail.com>
2018-10-01 15:52:09 +0100

    edited setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='maxlike',
-    version='2.0',
+    version='2.1',
+    author='joao ceia',
+    author_email='joao.p.ceia@gmail.com',
     packages=['maxlike'],
     url='https://github.com/jpceia/maxlike',
     license='',
-    author='jpceia',
-    author_email='joao.p.ceia@gmail.com',
     description="""
         Python module to fit statistical models to observed data through
-        maximum likelihood estimation."""
+        maximum likelihood estimation. """,
+    requires=[
+        "Numpy (>= 1.14.0)",
+        "Pandas (>= 0.23.0)",
+        "Scipy (>= 1.1.0)",
+        "Six (>= 1.11.0)",
+    ],
 )
