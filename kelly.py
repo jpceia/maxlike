a2cc366a6bba86acb57aa3fe03732eae3faf3081
Joao CEIA <joao.p.ceia@gmail.com>
2018-10-10 16:05:13 +0100

    converted skellam.py to analytics module

---
+++ kelly.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+
+def bernoulli(odd, prob, prob_err=None):
+    b = (odd * prob - 1) / (odd - 1)
+    k = 1
+    if prob_err is not None:
+        s = (1 + 1 / odd) * prob_err
+        k = b * b / (b * b + s * s)
+    return k * b
+
+
+def exclusive(o, p, dp_list=None):
+    """
+    Exclusive Kelly Algorithm:
+    1.  Calculated expected revenues:
+        E[r_i] = p_i * o_i
+    2.  Reorder the indexes so that the sequence E[r_i] is nonincreasing
+    3.  Set S = [], i = 1 and R = 1
+    4.  Repeat
+        if E[r_i] > R:
+            insert i in S
+            R := 1 - (sum_{not S} p_k) / (1 - sum_{S} (1 / o_i))
+        else:
+            break
+    5.  f_i := p_i - (1 / o_i) * (sum_{not S} p_k) / (1 - sum_{S} (1 / o_i))
+    """
+
+    if o.ndim == 1 and p.ndim == 1:
+        rev, q = p * o, 1 / o
+        idx = np.argsort(-rev)
+        tmp_p = 1 - np.cumsum(p[idx])
+        tmp_q = 1 - np.cumsum(q[idx])
+        R = np.insert(tmp_p / tmp_q, 0, 1, 0)[:-1]
+        i = np.argmin(rev[idx] > R)
+        b = np.maximum((rev - R[i]) * q, 0)
+        k = 1
+        if dp_list is not None:
+
+            if not isinstance(dp_list, (list, tuple)):
+                dp_list = [dp_list]
+
+            db_list = []
+            for dp in dp_list:
+                dR = np.insert(-np.cumsum(dp[idx]) / tmp_q, 0, 1, 0)[:-1]
+                db = dp - dR[i] / o
+                db[b == 0] = 0
+                db_list.append(db)
+
+            db = np.stack(db_list, -2)
+            D = 1 - b.sum() + b * o
+            M = np.diag(o) - 1
+            H = -(M[None, :, :] * (p / D / D)[None, None, :] * M[:, None, :]).sum(-1)
+            B = (db[:, None, :, None] * H[None, None, :, :] *
+                 db[None, :, None, :]).sum((-1, -2))
+            A = (b[:, None] * H * b[None, :]).sum((-1, -2))
+            k = A / (A + B.sum((-1, -2)))
+
+        return k * b
+
+    elif o.ndim > 1 and p.ndim > 1:
+        assert o.shape == p.shape
+        rev, q = p * o, 1 / o
+        idx = np.argsort(-rev, 1)
+        rng = np.arange(p.shape[0])[:, None]
+        tmp_p = 1 - np.cumsum(p[rng, idx], 1)
+        tmp_q = 1 - np.cumsum(q[rng, idx], 1)
+        R = np.insert(tmp_p / tmp_q, 0, 1, 1)[..., :-1]
+        i = np.argmin(rev[rng, idx] > R, 1)[:, None]
+        b = np.maximum((rev - R[rng, i]) * q, 0)
+        k = np.array([1])
+        if dp_list is not None:
+            
+            if not isinstance(dp_list, (list, tuple)):
+                dp_list = [dp_list]
+
+            db_list = []
+            for dp in dp_list:
+                tmp_dp = -np.cumsum(dp[rng, idx], 1)
+                dR = np.insert(tmp_dp / tmp_q, 0, 1, 1)[..., :-1]
+                db = dp - dR[rng, i] / o
+                db[b == 0] = 0
+                db_list.append(db)
+
+            db = np.stack(db_list, -2)
+            D = 1 - b.sum(-1)[:, None] + b * o
+            M = o[:, None, :] * np.diag(np.ones(o.shape[-1])) - 1
+            H = -(M[:, None, :] * (p / D / D)[:, None, None] * M[:, :, None]).sum(-1)
+            B = (db[:, :, None, :, None] * H[:, None, None, :, :] *
+                 db[:, None, :, None, :]).sum((-1, -2))
+            A = (b[:, :, None] * H * b[:, None, :]).sum((-1, -2))
+            k = A / (A + B.sum((-1, -2)))
+
+        return k[:, None] * b
+
+    elif o.ndim == 1 and p.ndim > 1:
+        # TODO
+        raise NotImplementedError
+
+    raise NotImplementedError
+
+
+def lasso_optimizer(q, X, y, tol=1e-8, reg=1):
+    if len(X.shape) < 2:
+        X = X.reshape(-1, 1)
+    a = np.zeros(X.shape[1])
+    max_steps = 500
+    for _ in range(max_steps):
+        y_pred = (a * X).sum(1)
+        grad = ((y / (q + y_pred) -
+                (1 - y) / (1 - y_pred))[:, None] * X).sum(0)
+        hess = ((y / (q + y_pred) ** 2 +
+                (1 - y) / (1 - y_pred) ** 2)[:, None] * X * X).sum(0)
+        step = (grad - reg) / hess
+        a += step
+        filt = a * hess < reg  # force to be positive with regularization
+        a[filt] = 0
+        if np.linalg.norm(step[~filt]) < tol:
+            break
+    else:
+        raise ValueError
+    # y_pred = (np.array([1, 0]) * X).sum(1)
+    # C = (y * np.log(1 + y_pred / q) - (1 - y) * np.log(1 - y_pred)).sum()
+    return a
---
+++ skellam.py
@@ -0,0 +1,202 @@
+import numpy as np
+from math import exp
+from scipy.special import ndtri, factorial
+from scipy.stats.mvn import mvnun
+
+
+def vectorize(n_in, n_out):
+    def wrap(foo):
+        return np.frompyfunc(foo, n_in, n_out)
+    return wrap
+
+
+@vectorize(3, 1)
+def gauss_bivar(x, y, rho):
+    return mvnun(-999 * np.ones((2)), (x, y), (0, 0), np.array([[1, rho], [rho, 1]]))[0]
+
+
+def poisson_vector(a, size=10):
+    curve = np.vectorize(lambda u: a**u / factorial(u))(np.arange(size))
+    dcurve = np.append(-1, curve[:-1] - curve[1:])
+    return exp(-a) * np.array([curve, dcurve])
+
+
+def nd_poisson_vector(a, size=10):
+    rng = np.arange(size)
+    curve = (a[..., None] ** rng) / factorial(rng)
+    dcurve = np.insert(curve[..., :-1] - curve[..., 1:], 0, -1, -1)  # arr, pos, value, axis
+    return np.exp(-a)[..., None] * np.stack([curve, dcurve])
+
+
+def nd_poisson_hess(a, size=10):
+    rng = np.arange(size)
+    curve = (a[..., None] ** rng) / factorial(rng)
+    dcurve = np.insert(curve[..., :-1] - curve[..., 1:], 0, -1, -1)
+    ddcurve = np.insert(-1, curve[..., :-2] - 2 * curve[..., 1:-1] + curve[..., 2:], 0, -1, -1)
+    return exp(-a)[..., None] * np.stack([curve, dcurve, ddcurve])
+
+
+def skellam_frame(a, b, size=10):
+    Poi_a, dPoi_a = poisson_vector(a, size)
+    Poi_b, dPoi_b = poisson_vector(b, size)
+    sk_frame = [Poi_a[:, None] * Poi_b[None, :]]
+    sk_frame += [dPoi_a[:, None] * Poi_b[None, :]]
+    sk_frame += [Poi_a[:, None] * dPoi_b[None, :]]
+    return sk_frame
+
+
+def nd_skellam_frame(a, b, size=10):
+    # assert a.shape == b.shape
+    Poi_a, dPoi_a = nd_poisson_vector(a, size)
+    Poi_b, dPoi_b = nd_poisson_vector(b, size)
+    sk_frame = [Poi_a[..., None] * Poi_b[..., None, :]]
+    sk_frame += [dPoi_a[..., None] * Poi_b[..., None, :]]
+    sk_frame += [Poi_a[..., None] * dPoi_b[..., None, :]]
+    return sk_frame
+
+
+def nd_skellam_hess(a, b, size=10):
+    Poi_a, dPoi_a, ddPoi_a = nd_poisson_hess(a, size)
+    Poi_b, dPoi_b, ddPoi_b = nd_poisson_hess(b, size)
+    sk_frame = [Poi_a[..., None] * Poi_b[..., None, :]]
+    sk_frame += [dPoi_a[..., None] * Poi_b[..., None, :]]
+    sk_frame += [Poi_a[..., None] * dPoi_b[..., None, :]]
+    sk_frame += [ddPoi_a[..., None] * Poi_b[..., None, :]]
+    sk_frame += [dPoi_a[..., None] * dPoi_b[..., None, :]]
+    sk_frame += [Poi_a[..., None] * ddPoi_b[..., None, :]]
+    return sk_frame
+
+
+# scipy.special.pdtr(n, lambda)
+def poisson_cdf(a, n):
+    # assert isinstance(n, int) and n >= 0
+    return poisson_vector(a, n + 1).sum(1)
+
+
+def tol2size(tol=1e-8, *args):
+    if len(args) == 0:
+        args = [1]  # standard value
+    return int(-ndtri(tol) * sum(map(lambda x: x ** .5, args)) + sum(args)) + 1
+
+
+def skellam_cdf(a, b, n, tol=1e-8, inverse=False):
+    # assert isinstance(n, int)
+    size = tol2size(tol, a, b)
+    rng = np.arange(size)
+    xy = rng[None, :] - rng[:, None]
+    xy = (xy >= n) if inverse else (xy <= n)
+    return np.asarray(map(lambda v: v[xy].sum(), skellam_frame(a, b, size)))
+
+
+def skellam_cdf_pair(a, b, n=0, tol=1e-8):
+    assert isinstance(n, int)
+    size = tol2size(tol, a, b)
+    rng = np.arange(size)
+    xy = rng[None, :] - rng[:, None]
+    s = skellam_frame(a, b, size)
+    res = np.asarray(list(zip(*[(v[xy < n].sum(), v[xy > n].sum()) for v in s])))
+    return res
+
+
+# pdtri(n, target) = poisson_cdf_root
+def poisson_cdf_root(target, n=2, tol=1e-8):
+    """
+    Finds the parameter 'a' of a Poisson distribution X in order to have
+    P(X <= n) = target
+    or P(X < n + 1/2 ) = target
+    The first guess is estimated through an approximation to the normal
+    distribution:
+    (n + 1/2 - a) / sqrt(a) = phi^-1(n) = phi
+    a + sqrt(a) * phi - n - 1/2 = 0
+
+    Then:
+        a = ((sqrt(phi ** 2 + 4 * n + 2) - phi) / 2) ** 2
+    """
+    # if not (isinstance(target, float) and target < 1 or target > 0):
+    if target >= 1 or target <= 0:
+        return np.NaN
+    phi = ndtri(target)
+    min_a = 1e-3
+    a = max(((phi * phi + 4 * n + 2) ** .5 - phi) ** 2 / 4, min_a)
+    e = 1
+    count = 0
+    max_steps = 50
+    while abs(e) > abs(tol):
+        f, df = poisson_cdf(a, n)
+        e = target - f
+        step = e / df
+        scale_factor = 1
+        new_a = a + scale_factor * step
+        new_f = f
+        new_e = abs(target - new_f)
+        for i in range(10):
+            if new_a > 0 and new_e < abs(e):
+                a = new_a
+                break
+            else:
+                new_a = a + scale_factor * step
+                new_f, _ = poisson_cdf(new_a, n)
+                new_e = abs(target - new_f)
+                scale_factor *= .5
+        count += 1
+        if count > max_steps:
+            return np.NaN
+    return a
+
+
+def skellam_cdf_root(target1, target2, n=0, tol=1e-8):
+    """
+    Finds the parameters (a, b) of a Skellam distribution in order to have
+    target1 = P(X-Y < n)
+    target2 = P(X-Y > n)
+    The first guess is estimated through an approximation to the normal
+    distribution
+    X-Y ~ N(a-b, a+b)
+    then:
+        (n - 1/2 - (a-b))/sqrt(a+b) = phi^-1(target1) =: phi_u
+        (n + 1/2 - (a-b))/sqrt(a+b) = phi^-1(1-target2) =: phi_d
+
+    n - 1/2 - (a-b) = sqrt(a+b) * phi_u
+    n + 1/2 - (a-b) = sqrt(a+b) * phi_d
+
+    sum_ab := 1 / (phi_d - phi_u)^2
+    diff_ab := .5 * phi_d / (phi_d - phi_u) - n
+
+    """
+    # if not (isinstance(target1, float) and isinstance(target2, float) and
+    #        target1 > 0 and target2 > 0 and target1 + target2 < 1):
+    if target1 + target2 >= 1 or min(target1, target2) <= 0:
+        return [np.NaN, np.NaN]
+    phi_d = ndtri(target1)
+    phi_u = ndtri(1 - target2)
+    sum_ab = 1 / (phi_u - phi_d) ** 2
+    diff_ab = 0.5 * (phi_u + phi_d) / (phi_u - phi_d) - n
+    floor = 1e-6
+    a = max((sum_ab + diff_ab) * 0.5, floor)
+    b = max((sum_ab - diff_ab) * 0.5, floor)
+    target = np.array([target1, target2])
+    e = 1
+    count = 0
+    max_steps = 50
+    while e > abs(tol):
+        skellam = skellam_cdf_pair(a, b, n, tol)
+        s, ds = skellam[:, 0], skellam[:, [1, 2]]
+        e = np.linalg.norm(target - s)
+        step = np.linalg.solve(ds, target - s)
+        scale_factor = 1
+        new_ab = np.maximum((a, b) + scale_factor * step, floor)
+        new_s = s
+        new_e = np.linalg.norm(new_s - target)
+        for i in range(10):
+            if all(new_ab > 0) and new_e < e:
+                a, b = new_ab
+                break
+            else:
+                new_ab = np.maximum((a, b) + scale_factor * step, floor)
+                new_s = skellam_cdf_pair(new_ab[0], new_ab[1], n, tol)[:, 0]
+                new_e = np.linalg.norm(new_s - target)
+                scale_factor *= .5
+        count += 1
+        if count > max_steps:
+            return [np.NaN, np.NaN]
+    return a, b
