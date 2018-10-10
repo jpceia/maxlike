5b33f3490a6465bc6706edc2072bede0a99fe83b
Joao CEIA <joao.p.ceia@gmail.com>
2018-10-10 16:12:25 +0100

    corrected import references in analytics/__init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from proba import proba
-from kelly import bernoulli, exclusive, lasso_regression
-from skellam import tol2size, skellam_cdf_root, skellam_frame
+from .proba import proba
+from .kelly import bernoulli, exclusive, lasso_regression
+from .skellam import tol2size, skellam_cdf_root, skellam_frame
