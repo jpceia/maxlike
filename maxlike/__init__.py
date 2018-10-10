5574c32171d096545cdd93bc3cd8fa39ffd5477c
Joao CEIA <joao.p.ceia@gmail.com>
2018-10-10 18:26:30 +0100

    added copula to root modules

--- maxlike/__init__.py
+++ maxlike/__init__.py
@@ -1,5 +1,6 @@
 from .tensor import Tensor
 from .maxlike import Poisson, Logistic, Finite, NegativeBinomial
 from .maxlike_base import ConvergenceError
+from .func import copula
 from . import func
 from . import utils
