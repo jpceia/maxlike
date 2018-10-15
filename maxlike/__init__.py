341d75aae2e1ff80ea0c0e3573a57ae709a009c7
jpceia <joao.p.ceia@gmail.com>
2018-10-15 07:40:52 +0100

    add Param to exported classes

--- maxlike/__init__.py
+++ maxlike/__init__.py
@@ -1,6 +1,6 @@
 from .tensor import Tensor
 from .maxlike import Poisson, Logistic, Finite, NegativeBinomial
-from .maxlike_base import ConvergenceError
+from .maxlike_base import ConvergenceError, Param
 from .func import copula
 from . import func
 from . import utils
