b000c4199ec0de706a3676398a1b9f9790fe0d0b
jpceia <joao.p.ceia@gmail.com>
2018-12-01 18:16:57 +0000

    added linear_regression.py with linear regression models under gaussian and poisson distributions.

---
+++ maxlike/linear_regression.py
@@ -0,0 +1,90 @@
+import abc
+import numpy as np
+
+
+class BaseLinearRegression(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, tol=1e-8, max_steps=20, verbose=False):
+        self.tol = tol
+        self.max_steps = max_steps
+        self.verbose = verbose
+
+    def y_pred(self, a, X):
+        return (a * X).sum(1)
+
+    def L(self, X, y, y_pred):
+        raise NotImplementedError
+
+    def grad(self, X, y, y_pred):
+        raise NotImplementedError
+
+    def hess(self, X, y, y_pred):
+        raise NotImplementedError
+
+    def param_guess(self, X, y):
+        return np.linalg.solve(X.transpose().dot(X), X.transpose().dot(y))
+
+    def fit(self, X, y):
+        if X.ndim < 2:
+            X = X.reshape(-1, 1)
+        a = self.param_guess(X, y)
+        y_pred = self.y_pred(a, X)
+        L = self.L(X, y, y_pred) / X.shape[0]
+        if self.verbose:
+            print("step {}: L={}".format(0, -L))
+        for k in range(self.max_steps):
+            step = np.linalg.solve(
+                self.hess(X, y, y_pred),
+                self.grad(X, y, y_pred))
+            m = 1
+            for _ in range(50):
+                a_new = a - m * step
+                y_pred = self.y_pred(X, a_new)
+                L_new = self.L(X, y, y_pred) / X.shape[0]
+                if L_new >= L:
+                    L = L_new
+                    if self.verbose:
+                        print("step {}: L={}".format(k + 1, -L))
+                    break
+                m *= .5
+            else:
+                raise ValueError
+            if np.linalg.norm(a - a_new) < self.tol:
+                break
+            a = a_new
+        else:
+            raise ValueError
+        self.a_ = a
+        return self
+
+    def predict(X):
+        if len(X.shape < 2):
+            X = X.reshape(-1, 1)
+        return self.y_pred(a, X)
+
+
+class PoissonRegression(BaseLinearRegression):
+
+    def y_pred(self, a, X):
+        return (a * X).sum(1)
+
+    def L(self, X, y, y_pred):
+        return (y * np.log(y_pred) - y_pred).sum()
+
+    def grad(self, X, y, y_pred):
+        return ((1 - y / y_pred)[:, None] * X).sum(0)
+
+    def hess(self, X, y, y_pred):
+        return ((y / y_pred / y_pred)[:, None, None] *
+                 X[:, None, :] * X[:, :, None]).sum(0)
+
+
+class GaussianRegression(BaseLinearRegression):
+
+    def fit(self, X, y):
+        self.a_ = self.param_guess(X, y)
+        return self
+
+    def predict(self, X):
+        return self.y_pred(self.a_, X)
