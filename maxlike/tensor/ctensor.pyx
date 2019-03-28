e6f62c22f3ac4f08151aa97335144c76ea877e08
jpceia <joao.p.ceia@gmail.com>
2019-03-28 04:14:36 +0000

    improvements in cython functions

--- maxlike/tensor/ctensor.pyx
+++ maxlike/tensor/ctensor.pyx
@@ -9,7 +9,7 @@ cdef np.ndarray last_diag(np.ndarray arr, int axis1, int axis2):
            swapaxes(-1, axis2 - 1)
 
 
-cpdef np.ndarray arr_take_diag(np.ndarray arr, int q, int p, mapping):
+cpdef np.ndarray arr_take_diag(np.ndarray arr, int q, int p, array mapping):
     cdef int k, f
     k = q
     for f in mapping:
@@ -21,29 +21,31 @@ cpdef np.ndarray arr_take_diag(np.ndarray arr, int q, int p, mapping):
 
 
 cpdef np.ndarray arr_expand_diag(np.ndarray arr, int p, array map1, array map2):
-    cdef int i, j, n
+    cdef int i, j, n, c
     cdef int[:] idx = np.ones(arr.ndim, dtype=np.int)
-    if map2:
-        for i, j in zip(map1, map2):
-            if i != j:
-                n = arr.shape[p + i]
-                idx[p + i] = n
-                idx[p + j] = n
-                arr = arr * np.eye(n).reshape(idx)
-                idx[p + i] = 1
-                idx[p + j] = 1
+    k = 0
+    for j in map2:
+        i = map1[k]
+        k += 1
+        if i == j:
+            continue
+        n = arr.shape[p + i]
+        idx[p + i] = n
+        idx[p + j] = n
+        arr = arr * np.eye(n).reshape(idx)
+        idx[p + i] = 1
+        idx[p + j] = 1
     return arr
 
 
 cpdef array compose_mappings(array map1, array map2):
     cdef int k
     cdef array result = array('b')
-    if map2:
-        for k in map2:
-            if k < 0:
-                result.append(-1)
-            else:
-                result.append(map1[k])
+    for k in map2:
+        if k < 0:
+            result.append(-1)
+        else:
+            result.append(map1[k])
     return result
 
 
