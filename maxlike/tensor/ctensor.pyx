3420dc7219b77c7bede149372e6b0aa32b6ee42d
jpceia <joao.p.ceia@gmail.com>
2019-04-06 19:31:12 +0100

    improvements in cython code

--- maxlike/tensor/ctensor.pyx
+++ maxlike/tensor/ctensor.pyx
@@ -3,76 +3,78 @@ cimport numpy as np
 from array import array
 from cpython.array cimport array
 
-
-cdef np.ndarray last_diag(np.ndarray arr, int axis1, int axis2):
-    return np.diagonal(arr, axis1=axis1, axis2=axis2).\
-           swapaxes(-1, axis2 - 1)
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+np.import_array()
 
 
-cpdef np.ndarray arr_take_diag(np.ndarray arr, int q, int p, array mapping):
-    cdef int k, f
-    k = q
-    for f in mapping:
+
+cpdef np.ndarray arr_take_diag(np.ndarray arr, char q, char p, char[:] mapping):
+    cdef char k, f
+    for k in range(len(mapping)):
+        f = mapping[k]
         if f < 0:
             continue
-        arr = np.expand_dims(last_diag(arr, k, p + f), k)
-        k += 1
+        arr = np.PyArray_Diagonal(arr, 0, q + k, p + f)  # np.diagonal
+        arr = arr.swapaxes(-1, p + f - 1)
+        arr = np.expand_dims(arr, k)
     return arr
 
 
-cpdef np.ndarray arr_expand_diag(np.ndarray arr, int p, array map1, array map2):
-    cdef int i, j, n, c
+cpdef np.ndarray arr_expand_diag(np.ndarray arr, char p, char[:] map1, char[:] map2):
+    cdef char i, j
+    cdef int n
     cdef int[:] idx = np.ones(arr.ndim, dtype=np.int)
-    k = 0
-    for j in map2:
+    for k in range(len(map2)):
         i = map1[k]
-        k += 1
+        j = map2[k]
         if i == j:
             continue
         n = arr.shape[p + i]
         idx[p + i] = n
         idx[p + j] = n
-        arr = arr * np.eye(n).reshape(idx)
+        arr = arr * np.broadcast_to(np.eye(n), idx)
         idx[p + i] = 1
         idx[p + j] = 1
     return arr
 
 
-cpdef array compose_mappings(array map1, array map2):
-    cdef int k
+cpdef array compose_mappings(char[:] map1, char[:] map2):
+    cdef char k, f
     cdef array result = array('b')
-    for k in map2:
-        if k < 0:
+    for k in range(len(map2)):
+        f = map2[k]
+        if f < 0:
             result.append(-1)
         else:
-            result.append(map1[k])
+            result.append(map1[f])
     return result
 
 
-cpdef np.ndarray arr_swapaxes(np.ndarray arr, int q, int p, array mapping):
-    cdef int k, f
-    k = q
-    for f in mapping:
+cpdef np.ndarray arr_swapaxes(np.ndarray arr, char q, char p, char[:] mapping):
+    cdef char k, f
+    for k in range(len(mapping)):
+        f = mapping[k]
         if f >= 0:
-            arr = arr.swapaxes(k, p + f)
-        k += 1
+            arr = arr.swapaxes(q + k, p + f)
     return arr
 
 
-cdef np.ndarray arr_swapaxes_cross(np.ndarray arr, int p, array map1, array map2):
-    cdef k, f, l, n
+cdef np.ndarray arr_swapaxes_cross(np.ndarray arr, char p, char[:] map1, char[:] map2):
+    cdef char k, f, l
+    cdef int N = len(map1)
+    cdef int n
     cdef int[:] idx = np.ones(arr.ndim, dtype=np.int)
-    k = p
-    for f in map2:
-        n = arr.shape[k - p]
-        if map1 and f in map1:
-            l = map1.index(f)
-            idx[l] = n
-            idx[k] = n
-            val = val * np.eye(n).reshape(idx)
-            idx[l] = 1
-            idx[k] = 1
+    for k in range(len(map2)):
+        f = map2[k]
+        for l in range(N):
+            if map1[l] == f:
+                n = arr.shape[k]
+                idx[l] = n
+                idx[p + k] = n
+                val = val * np.broadcast_to(np.eye(n), idx)
+                idx[l] = 1
+                idx[p + k] = 1
+                break
         else:
-            arr = arr.swapaxes(k, p + f)
-        k += 1
+            arr = arr.swapaxes(p + k, p + f)
     return arr
