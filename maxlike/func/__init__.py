51bd873f8b7d5d41cf948efeaa27248249fb32b7
jpceia <joao.p.ceia@gmail.com>
2019-05-16 00:33:14 +0100

    moved Logistic(Func) to sigmoid.py

--- maxlike/func/__init__.py
+++ maxlike/func/__init__.py
@@ -1,5 +1,6 @@
 from .func_base import Affine, Compose
 from .func_op import FuncWrap, Sum, Product, Collapse
 from .func_proba import Poisson, NegativeBinomial
-from .func import Linear, Quadratic, X, Constant, Scalar, Vector, Exp, Logistic
+from .func import Linear, Quadratic, X, Constant, Scalar, Vector, Exp
 from .matrix import MarkovMatrix, CollapseMatrix
+from .sigmoid import Logistic
---
+++ maxlike/func/sigmoid.py
@@ -0,0 +1,27 @@
+import numpy as np
+from ..tensor import Tensor
+from .func_base import Func, grad_tensor, hess_tensor
+from scipy.special import expit
+
+
+class Logistic(Func):
+
+    def __call__(self, params):
+        return expit(params[0])
+
+    def grad(self, params, i):
+        f = expit(params[0])
+        return grad_tensor(f * (1 - f), params, i, True)
+
+    def hess(self, params, i, j):
+        f = expit(params[0])
+        return hess_tensor(f * (1 - f) * (1 - 2 * f),
+                           params, i, j, True, True)
+
+    def eval(self, params):
+        f = expit(params[0])
+        return Tensor(f), \
+               [grad_tensor(f * (1 - f), params, 0, True)], \
+               [[hess_tensor(f * (1 - f) * (1 - 2 * f),
+                             params, 0, 0, True, True)]]
+
