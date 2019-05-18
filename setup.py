04176fd740ce8bfe25a8e6ecd77d23e6bd136058
jpceia <joao.p.ceia@gmail.com>
2019-05-18 19:02:47 +0100

    updated setup file

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 import os
+import numpy as np
 from setuptools import setup
 from Cython.Build import cythonize
 
 
-__version__ = '2.3.1'
+__version__ = '2.3.3'
 __package__ = 'maxlike'
 
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-
 with open("README.md") as f:
     __doc__ = f.read()
-
-
-def numpy_include():
-    import numpy
-    return numpy.get_include()
-
 
 
 setup(
@@ -29,8 +23,10 @@ setup(
     packages=['maxlike', 'maxlike.tensor', 'maxlike.func', 'maxlike.analytics'],
     url='https://github.com/jpceia/maxlike',
     license='',
-    description=__doc__,
+    description="Python package to model count data",
+    long_description=__doc__,
+    long_description_content_type="text/markdown",
     install_requires=required,
-    include_dirs = [numpy_include()],
+    include_dirs = [np.get_include()],
     ext_modules=cythonize("maxlike/tensor/ctensor.pyx")
 )
