1fb488b678945f3d4573f6569d7922b458b3b0d3
jpceia <joao.p.ceia@gmail.com>
2019-05-20 00:28:01 +0100

    changes to have maxlike ready for Pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 
-__version__ = '2.3.5'
+__version__ = '2.3.6'
 __package__ = 'maxlike'
 
 here = os.path.dirname(os.path.abspath(__file__))
@@ -21,20 +21,24 @@ setup(
     version=__version__,
     author='joao ceia',
     author_email='joao.p.ceia@gmail.com',
-    download_url='https://github.com/jpceia/maxlike/archive/v{}.tar.gz'.format(__version__),
     packages=['maxlike', 'maxlike.tensor', 'maxlike.func', 'maxlike.analytics'],
     url='https://github.com/jpceia/maxlike',
-    license='',
+    license='nolicense',
     description="Python package to model count data",
     long_description=__doc__,
     long_description_content_type="text/markdown",
     install_requires=required,
+    tests_require=["nose", "coverage"],
+    test_suite="nose.collector",
     include_dirs = [np.get_include()],
-    ext_modules=cythonize("maxlike/tensor/ctensor.pyx"),
+    ext_modules=cythonize(
+        os.path.join(here, "maxlike/tensor/ctensor.pyx")),
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
-        ]
+    ]
 )
