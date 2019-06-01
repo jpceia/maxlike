08fc2219ebc9cd1458c65080d1bff63a45dd1c95
jpceia <joao.p.ceia@gmail.com>
2019-06-01 02:17:59 +0100

    completed NegativeBinomial Func

--- maxlike/func/func_proba.py
+++ maxlike/func/func_proba.py
@@ -48,7 +48,7 @@ class Poisson(Func):
         return val, [grad], [[hess]]
 
 
-class NegativeBinomial(Func):
+class LogNegativeBinomial(Func):
 
     def __init__(self, size=10, r=1):
         self.size = size
@@ -56,29 +56,57 @@ class NegativeBinomial(Func):
 
     def __call__(self, params):
         """
-        exp(
-            gammaln(r + x) - gammaln(r) - ln x! +
-            x ln(m) + r ln(r) - (x + r) * ln(r + m))
-            )
+        gammaln(r + x) - gammaln(r) - ln x! +
+        x ln(m) + r ln(r) - (x + r) * ln(r + m))
         """
-        m = np.asarray(params[0])
+        m = np.asarray(params[0])[..., None]
         x = np.arange(self.size)
-        vec = gammaln(self.r + x) - gammaln(self.r)
-        vec -=np.log(factorial(x))
-        vec += x * np.log(m) + self.r * np.log(self.r)
+        vec = x * np.log(m)
+        vec += gammaln(self.r + x)
+        vec -= gammaln(self.r)
+        vec += self.r * np.log(self.r)
+        vec -= np.log(factorial(x))
         vec -= (x + self.r) * np.log(self.r + m)
-        return Tensor(np.exp(vec), dim=1)
+        return Tensor(vec, dim=1)
 
     def grad(self, params, i):
         """
-        grad_f = ((x / m) - (x + r) / (r + m)) * f
+        grad_f = (x / m) - (x + r) / (r + m)
         """
-        m = np.asarray(params[0])
+        m = np.asarray(params[0])[..., None]
         x = np.arange(self.size)
-        vec = gammaln(self.r + x) - gammaln(self.r)
-        vec -=np.log(factorial(x))
-        vec += x * np.log(m) + self.r * np.log(self.r)
-        vec -= (x + self.r) * np.log(self.r + m)
         return grad_tensor(
-            np.exp(vec) * ((x / m) - (x + self.r) / (self.r + m)),
+            x / m - (x + self.r) / (self.r + m),
             params, i, True, dim=1)
+
+    def hess(self, params, i, j):
+        """
+        hess_f = (x + r) / (r + m) ** 2 - x / m ** 2
+        """
+        m = np.asarray(params[0])[..., None]
+        x = np.arange(self.size)
+        return hess_tensor(
+            (x + self.r) / np.square(self.r + m) - x / np.square(m),
+            params, i, j, True, True, dim=1)
+
+    def eval(self, params):
+        m = np.asarray(params[0])[..., None]
+        x = np.arange(self.size)
+        vec = x * np.log(m)
+        vec += gammaln(self.r + x)
+        vec -= gammaln(self.r)
+        vec += self.r * np.log(self.r)
+        vec -= np.log(factorial(x))
+        vec -= (x + self.r) * np.log(self.r + m)
+        val = Tensor(vec, dim=1)
+        grad = grad_tensor(
+            x / m - (x + self.r) / (self.r + m),
+            params, 0, True, dim=1)
+        hess = hess_tensor(
+            (x + self.r) / np.square(self.r + m) - x / np.square(m),
+            params, 0, 0, True, True, dim=1)
+        return val, [grad], [[hess]]
+
+
+def NegativeBinomial(**kwargs):
+    return Exp() @ LogNegativeBinomial(**kwargs)
