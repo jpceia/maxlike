eb6088bf3bc14c54be67abec0e93b58c5c548aca
jpceia <joao.p.ceia@gmail.com>
2019-06-15 23:44:23 +0100

    added MarkovVector and corresponding unittest

--- maxlike/func/__init__.py
+++ maxlike/func/__init__.py
@@ -2,5 +2,5 @@ from .func_base import Affine, Compose
 from .func_op import FuncWrap, Sum, Product, Collapse
 from .func_proba import Poisson, LogNegativeBinomial, NegativeBinomial
 from .func import Linear, Quadratic, X, Constant, Scalar, Vector, Exp, Log
-from .matrix import MarkovMatrix, CollapseMatrix
+from .matrix import MarkovMatrix, MarkovVector, CollapseMatrix
 from .sigmoid import Sigmoid, Logistic, ArcTan
--- maxlike/func/matrix.py
+++ maxlike/func/matrix.py
@@ -167,3 +167,129 @@ class MarkovMatrix(Func):
                [[hess_tensor(H11, params, 0, 0, True, True, dim=2)],
                 [hess_tensor(H12, params, 0, 1, True, True, dim=2),
                  hess_tensor(H22, params, 1, 1, True, True, dim=2)]]
+
+
+class MarkovVector(Func):
+
+    def __init__(self, size=8, steps=18, skew=None):
+        assert size > 0
+        self.size = size
+        self.steps = steps
+        if skew is None:
+            skew = np.ones((2 * size - 1,))
+        self.skew = skew
+
+    def __call__(self, params):
+        s1 = np.asarray(params[0] / self.steps)
+        s2 = np.asarray(params[1] / self.steps)
+        assert s1.shape == s2.shape
+        assert np.all(s1 > 0)
+        assert np.all(s2 > 0)
+        assert np.all(s1 + s2 < 1)
+        M = np.zeros(s1.shape + (2 * self.size - 1,))
+        M[..., self.size - 1] = 1
+        for step in range(self.steps):
+            e = 0
+            for x in range(2 * self.size - 1):
+                x_ = max(x - 1, 0)
+                f1 = self.skew[x]
+                f2 = self.skew[2 * self.size - 2 - x]
+                m = M[..., x].copy()
+                M[..., x_] += m * s2 * f2
+                M[..., x]  -= m * (s1 * f1 + s2 * f2)
+                M[..., x]  += e # from prev item
+                e = m * s1 * f1
+            M[..., -1] += e
+
+        return Tensor(M, dim=1)
+
+    def grad(self, params, i):
+        return self.eval(params)[1][i]
+
+    def hess(self, params, i, j):
+        return self.eval(params)[2][i][j]
+
+    def eval(self, params):
+        s1 = np.asarray(params[0] / self.steps)
+        s2 = np.asarray(params[1] / self.steps)
+        assert s1.shape == s2.shape
+        assert np.all(s1 > 0)
+        assert np.all(s2 > 0)
+        assert np.all(s1 + s2 < 1)
+        M = np.zeros(s1.shape + (2 * self.size - 1, ))
+
+        D1 = np.zeros_like(M)
+        D2 = np.zeros_like(M)
+        H11 = np.zeros_like(M)
+        H12 = np.zeros_like(M)
+        H22 = np.zeros_like(M)
+
+        M[..., self.size - 1] = 1
+        for step in range(self.steps):
+            e = 0
+            e1 = 0
+            e2 = 0
+            e11 = 0
+            e12 = 0
+            e22 = 0
+            for x in range(2 * self.size - 1):
+                x_ = max(x - 1, 0)
+                f1 = self.skew[x]
+                f2 = self.skew[2 * self.size - 2 - x]
+
+                m = M[..., x].copy()
+                d1 = D1[..., x].copy()
+                d2 = D2[..., x].copy()
+                h11 = H11[..., x].copy()
+                h22 = H22[..., x].copy()
+                h12 = H12[..., x].copy()
+
+                M[..., x_] += m * s2 * f2
+                M[..., x]  -= m * (s1 * f1 + s2 * f2)
+                M[..., x]  += e # from prev item
+                e = m * s1 * f1
+
+                D1[..., x_] += d1 * s2 * f2
+                D1[..., x]  -= d1 * (s1 * f1 + s2 * f2) + m * f1
+                D1[..., x]  += e1
+                e1 = (m + d1 * s1) * f1
+
+                D2[..., x_] += (m + d2 * s2) * f2
+                D2[..., x]  -= d2 * (s1 * f1 + s2 * f2) + m * f2
+                D2[..., x]  += e2
+                e2 = d2 * s1 * f1
+
+                H11[..., x_] += h11 * s2 * f2
+                H11[..., x]  -= h11 * (s1 * f1 + s2 * f2) + 2 * d1 * f1
+                H11[..., x]  += e11
+                e11 = (2 * d1 + h11 * s1) * f1
+
+                H12[..., x_] += h12 * s2 * f2 + d1 * f2
+                H12[..., x]  -= h12 * (s1 * f1 + s2 * f2) + d2 * f1 + d1 * f2
+                H12[..., x]  += e12
+                e12 = (d2 + h12 * s1) * f1
+
+                H22[..., x_] += (2 * d2 + h22 * s2) * f2
+                H22[..., x]  -= h22 * (s1 * f1 + s2 * f2) + 2 * d2 * f2
+                H22[..., x]  += e22
+                e22 = h22 * s1 * f1
+
+            M[..., -1] += e
+            D1[..., -1] += e1
+            D2[..., -1] += e2
+            H11[..., -1] += e11
+            H12[..., -1] += e12
+            H22[..., -1] += e22
+
+        D1 /= self.steps
+        D2 /= self.steps
+        H11 /= self.steps * self.steps
+        H12 /= self.steps * self.steps
+        H22 /= self.steps * self.steps
+
+        return Tensor(M, dim=1), \
+               [grad_tensor(D1, params, 0, True, dim=1),
+                grad_tensor(D2, params, 1, True, dim=1)], \
+               [[hess_tensor(H11, params, 0, 0, True, True, dim=1)],
+                [hess_tensor(H12, params, 0, 1, True, True, dim=1),
+                 hess_tensor(H22, params, 1, 1, True, True, dim=1)]]
